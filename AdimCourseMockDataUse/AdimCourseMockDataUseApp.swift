ccbcce88ecfc803966cdc8789bad5df7f3ea94f0
GuruMahan <iosdeve2022@gmail.com>
2023-02-06 15:09:52 +0530

    Initial Commit

---
+++ AdimCourseMockDataUse/AdimCourseMockDataUseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdimCourseMockDataUseApp.swift
+//  AdimCourseMockDataUse
+//
+//  Created by Guru Mahan on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AdimCourseMockDataUseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
